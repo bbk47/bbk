0ee17cf4077ca6c17ae79934f6e6601afae398e5
xuxihai <xuxihai123@gmail.com>
2024-02-23 22:05:17 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
-bbk_VERSION=`git describe --tags --abbrev=0`
+# config===start
+zip_prefix=bbk
+output_dir="./output"
+output_bin=bbk
+# config===end
+
+APP_VERSION=`git describe --tags --abbrev=0`
 GIT_HASH=`git rev-parse --short HEAD`
-echo $bbk_VERSION'-'$GIT_HASH
+echo $APP_VERSION'-'$GIT_HASH
 
 cwd=`pwd`
 
 
-rm -rf ./output
-mkdir -p ./output
+
+rm -rf $output_dir
+mkdir -p $output_dir
 
 # os_all='linux windows darwin freebsd'
 # arch_all='386 amd64 arm arm64'
@@ -21,30 +28,29 @@ do
     os=${platform_split[0]}
     arch=${platform_split[1]}
 
-    targetzipname="bbk_${bbk_VERSION}_${os}_${arch}"
-    bbk_outdir="output/${targetzipname}"
-    output_name1=bbk
-    mkdir -p $bbk_outdir
+    targetzip_name="${zip_prefix}_${APP_VERSION}_${os}_${arch}"
+    build_outdir="${output_dir}/${targetzip_name}"
+    mkdir -p $build_outdir
+    platform_bin=$output_bin
     if [ $os = "windows" ]; then
-        output_name1+='.exe'
-        output_name2+='.exe'
+        platform_bin+='.exe'
     fi
-    echo "Build output/${targetzipname}...";\
-    env CGO_ENABLED=0 GOOS=${os} GOARCH=${arch} go build -trimpath -ldflags "-X main.Version=$bbk_VERSION -X main.GitCommitHash=$GIT_HASH" -o ${bbk_outdir}/${output_name1} ./main.go
-    echo "Build ${bbk_outdir} done";
+    echo "Build ${build_outdir}...";\
+    env CGO_ENABLED=0 GOOS=${os} GOARCH=${arch} go build -trimpath -ldflags "-X main.Version=$APP_VERSION -X main.GitCommitHash=$GIT_HASH" -o ${build_outdir}/${platform_bin} ./main.go
+    echo "Build ${build_outdir} done";
 
-    cp -rf ./etc ${bbk_outdir}
-    cp -rf ./examples ${bbk_outdir}
-    cp -rf ./daemon ${bbk_outdir}
+    cp -rf ./etc ${build_outdir}
+    cp -rf ./examples ${build_outdir}
+    cp -rf ./daemon ${build_outdir}
 
     # packages
-    cd output
+    cd $output_dir
     if [ $os = "windows" ]; then
-        zip -rq ${targetzipname}.zip ${targetzipname}
+        zip -rq ${targetzip_name}.zip ${targetzip_name}
     else
-        tar -zcf ${targetzipname}.tar.gz ${targetzipname}
+        tar -zcf ${targetzip_name}.tar.gz ${targetzip_name}
     fi  
-    rm -rf ${targetzipname}
+    rm -rf ${targetzip_name}
     cd ..
 done
 
